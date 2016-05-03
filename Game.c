d9ace5bc8e88f6d11cee1619329e48c9aeabec2b
Eric-Windsor <e.windsor512@gmail.com>
2016-05-03 20:21:27 +1000

    Update Game.c

--- Game.c
+++ Game.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct _stats{
+    int KPIs
+    int Campuses
+    int Group
+    int ARC Grants
+    int Patents
+    int Papers
+    int THDs
+    int BPSs
+    int BQNs
+    int MJs
+    int MTVs
+    int MMONEYs
+} stats;
