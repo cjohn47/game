c2ce78261d7c1e295ea5fa06e28fe436037a1543
Shariq-Khan <shariq.14628@gmail.com>
2016-05-04 20:58:37 +1000

    Update Game.c

--- Game.c
+++ Game.c
@@ -1,32 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct _stats{
-    int KPIs
-    int Campuses
-    int Group
-    int ARC Grants
-    int Patents
-    int Papers
-    int THDs
-    int BPSs
-    int BQNs
-    int MJs
-    int MTVs
-    int MMONEYs
-} stats;
-
-typedef struct _game {
-    int turnNumber; 
-    int whoseTurn;
-    int currentDice;
-    stats PlayerA;
-    stats PlayerB;
-    stats PlayerC;
-}
 
 makeAction(Game g, action a) {
-int currPlayer = getWhoseTurn(g);
+   int currPlayer = getWhoseTurn(g);
    
    if(a.actionCode == BUILD_CAMPUS) {
       //some code here
