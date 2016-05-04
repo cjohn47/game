968e2012614bc5135c4bfab4f20120442f4c66fe
Shariq-Khan <shariq.14628@gmail.com>
2016-05-04 20:57:09 +1000

    Update Game.c

--- Game.c
+++ Game.c
@@ -24,3 +24,43 @@ typedef struct _game {
     stats PlayerB;
     stats PlayerC;
 }
+
+makeAction(Game g, action a) {
+int currPlayer = getWhoseTurn(g);
+   
+   if(a.actionCode == BUILD_CAMPUS) {
+      //some code here
+   
+   
+   } else if (a.actionCode == BUILD_G08) {
+   
+   
+   
+   
+   } else if (a.actionCode == OBTAIN_ARC) {
+   
+   
+   
+   
+   } else if (a.actionCode == RETRAIN_STUDENTS) {
+   
+   
+   
+   
+   } else if (a.actionCode == OBTAIN_PUBLICATION) {
+   
+   
+   
+   
+   
+   } else if (a.actionCode == OBTAIN_IP_PUBLICATION) {
+   
+   
+   
+   
+   } else if (a.actionCode == )     
+    
+    
+    
+}
+}
