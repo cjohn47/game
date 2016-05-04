2c367ae71e381ea01e621017e7d268f831de95ac
Shariq-Khan <shariq.14628@gmail.com>
2016-05-04 21:02:24 +1000

    Update Game.c

--- Game.c
+++ Game.c
@@ -7,34 +7,32 @@ makeAction(Game g, action a) {
    
    
    } else if (a.actionCode == BUILD_G08) {
-   
+      //some code here
    
    
    
    } else if (a.actionCode == OBTAIN_ARC) {
-   
-   
+      //some code here
    
    
    } else if (a.actionCode == RETRAIN_STUDENTS) {
-   
+      //some code here 
    
    
    
    } else if (a.actionCode == OBTAIN_PUBLICATION) {
-   
+      //some code here
    
    
    
    
    } else if (a.actionCode == OBTAIN_IP_PUBLICATION) {
-   
+      //some code here
    
    
    
    } else if (a.actionCode == )     
+      //some code here
     
-    
-    
-}
+   }
 }
