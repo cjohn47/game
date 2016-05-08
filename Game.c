bbe9e8151395870bdc023f9f0a07aa754163d3f0
cjohn47 <chris.j1598@gmail.com>
2016-05-08 19:57:56 +1000

    Update Game.c

--- Game.c
+++ Game.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
+
+#define MAX_VERTICES 6
+#define MAX_ROWS 12
+#define MAX_ARCS_PER_ROW 8
+
+
+
+
 typedef struct _stats{
     int KPIs;
     int Campuses;
@@ -20,6 +31,8 @@ typedef struct _game {
     int currentTurn;
     int whoseTurn;
     int currentDice;
+    action currentAction;
+    
     
     //3 dimensional array
     //of the form [column][vertex][contents] specifiyng a point
@@ -27,7 +40,23 @@ typedef struct _game {
     //for rows 0 and 10 if [vertex] > 1 then point is outside board
     //for rows 1 and 9 if [vertex] > 3 then the point is outside the board
     //[contents] specifies what is on the point
-    int gameBoard[11][6][1];
+    //int gameBoard[11][6][1];
+    
+    /*2 dimensional array
+    //of the form [column][vertex] containing the contents of the vertex
+    //specifiyng a point
+    //Note that:
+    //for rows 0 and 10 if[vertex] > 1 then point is outside board
+    //for rows 1 and 9 if [vertex] > 3 then the point is outside the board
+    //
+    */
+    int boardVertices[MAX_ROWS]{MAX_VERTICES];
+    
+    /*2 dimensional array of the
+    //form [row][arc] conataining the contents of the arc.
+    */
+    int boardArcs[MAX_ROWS][MAX_ACRS_PER_ROW]
+    
     
     stats PlayerA;
     stats PlayerB;
