1ebe876d4874b53689c883cb6b96437e650c0e35
Jashvin1 <jtiwana@live.com>
2016-05-12 20:07:06 +1000

    Add files via upload

---
+++ GetFunctions.c
@@ -0,0 +1,133 @@
+//
+//  main.c
+//  GetFunctions
+//
+//  Created by Jashvin Tiwana on 10/05/2016.
+//  Copyright © 2016 Jashvin Tiwana. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Hello World!\n");
+    return 0;
+}
+
+
+/*
+ int getMostPublications (Game g)
+{
+    int university = -1;
+    int NUM_PUBS = 0;
+    int i = 0;
+    
+    while (i<NUM_UNIS) {
+        if ((g-> university[i] . papers + g-> university[i] . patents) > NUM_PUBS) {
+            university = i;
+        }
+        else if (NUM_PUBS == (g-> university[i] . papers + g-> university[i] . patents)) {
+            university = -1;
+        }
+        i++;
+    }
+    return university;
+}
+ 
+
+
+
+
+ int getMostARCs (Game g)
+{
+    int university = -1;
+    int NUM_ARCS = 0;
+    int i = 0;
+    
+    while (i<NUM_UNIS) {
+        if (g-> university[i] . ARCGrants > NUM_ARCS) {
+            university = i
+        }
+        else if (NUM_ARCS == (g-> university[i] . ARCGrants)) {
+            university = -1;
+        }
+        i++;
+    }
+    return university;
+    }
+}
+
+ int getTurnNumber (Game g)
+{
+
+    return g -> turnNumber;
+}
+
+int getWhoseTurn (Game g)
+{
+    return g -> whoseTurn;
+}
+
+ 
+int getDiceValue (Game g, int regionID)
+{
+    return g->currentDice;
+}
+
+ 
+int getCampuses(Game g, int player)
+{
+    int university = -1;
+    int i = 0;
+    
+    if (i<NUM_UNIS) {
+        return g-> university[i] . Campuses
+    }
+    i++
+}
+
+ 
+ 
+int getKPIpoints (Game g, int player){
+    int university = -1;
+    int i = 0;
+    
+    if (i<NUM_UNIS) {
+        return g-> university[i] . KPIs
+    }
+    i++
+}
+
+ 
+ 
+int getARCs (Game g, int player){
+    int university = -1;
+    int i = 0;
+    
+    if (i<NUM_UNIS) {
+        return g-> university[i] . ARCGrants
+    }
+    i++
+}
+
+ 
+ 
+int getPublications (Game g, int player){
+    int university = -1;
+    int i = 0;
+    
+    if (i<NUM_UNIS) {
+        return (g-> university[i] . Patents + g-> university[i].Papers)
+    }
+    i++
+}
+*/
+
+
+
+
+
+
